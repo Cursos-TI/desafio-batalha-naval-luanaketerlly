dd1e6cb7adfec49863a4d3d3449673097cbee384
luanaketerlly <luanaketerlly@gmail.com>
2025-08-11 03:38:05 +0000

    commit do tabuleiro simples

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,47 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+        //Declarando variáveis que representam Linhas (y) e colunas (x)    
+    int x,y;
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+       //Declarando valores contidos na primra linha da matriz.
+     char colunas [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G','H', 'I', 'J'};
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+       //Declarando tipo de variável e tamanho do tabuleiro
+     char tabuleiro [10] [10] = {
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '0', '0'},
+       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '3', '0'},
+       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '3', '0'},
+       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '3', '0'},
+       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '0', '0'},
+       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '0', '0'},
+       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '0', '0'},
+       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '0', '0'},
+       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '0', '0'},
+       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '0', '0'},
+    };   
+
+         //Início da condição para apresentar no console os valores determinados na matriz
+        printf("   ");
+            for (x = 0; x < 10; x++) {  //Adicionando incremento para exibir a sequência da primeira primeira linha
+                printf(" %c ", 'A' + x); 
+            }
+                printf("\n");
+
+            for (y = 0; y < 10; y++){
+                printf("%2d ", y);   //Código para exibir a primeira coluna enumerada do tabuleiro
+
+                for (x = 0; x < 10; x++) {
+                    printf(" %c ", tabuleiro [y][x]); //Percorre linhas e colunas
+                }
+                printf("\n");
+            }
+                
+
+
+
 
     return 0;
 }
