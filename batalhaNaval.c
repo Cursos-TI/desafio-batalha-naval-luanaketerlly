14cea7aab9121dedf169452f0521a66f347cbbc4
luanaketerlly <luanaketerlly@gmail.com>
2025-08-12 01:59:42 +0000

    Commit constante

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,28 @@
 #include <stdio.h>
 
+#define LINHAS 10
+#define COLUNAS 10
+
 int main() {
 
-        //Declarando variáveis que representam Linhas (y) e colunas (x)    
-    int x,y;
-
+    int matriz [LINHAS][COLUNAS];   
+    int i, j;
        //Declarando valores contidos na primra linha da matriz.
-     char colunas [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G','H', 'I', 'J'};
-
-       //Declarando tipo de variável e tamanho do tabuleiro
-     char tabuleiro [10] [10] = {
-
-       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '0', '0'},
-       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '3', '0'},
-       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '3', '0'},
-       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '3', '0'},
-       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '0', '0'},
-       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '0', '0'},
-       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '0', '0'},
-       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '0', '0'},
-       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '0', '0'},
-       {'0' ,'0' ,'0', '0', '0', '0', '0', '0', '0', '0'},
-    };   
+    int colunas [10] = {"A", "B", "C", "D", "E", "F", "G","H", "I", "J"};
+   
 
          //Início da condição para apresentar no console os valores determinados na matriz
         printf("   ");
-            for (x = 0; x < 10; x++) {  //Adicionando incremento para exibir a sequência da primeira primeira linha
-                printf(" %c ", 'A' + x); 
+           for (i = 0; i < LINHAS; i++) {  //Adicionando incremento para exibir a sequência da primeira primeira linha
+                printf(" %d ", 'A' + j); 
             }
                 printf("\n");
 
-            for (y = 0; y < 10; y++){
-                printf("%2d ", y);   //Código para exibir a primeira coluna enumerada do tabuleiro
+           for (j = 0; j < COLUNAS; j++){
+                printf("%2d ", j);   //Código para exibir a primeira coluna enumerada do tabuleiro
 
-                for (x = 0; x < 10; x++) {
-                    printf(" %c ", tabuleiro [y][x]); //Percorre linhas e colunas
+                for (j = 0; j < 10; j++) {
+                    printf(" %d ", matriz [i][j]); //Percorre linhas e colunas
                 }
                 printf("\n");
             }
